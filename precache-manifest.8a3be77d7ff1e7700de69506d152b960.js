3ea5851e044a530a4a5c947f5e0e941741c0b04b
gems.sirawit <sirawit.james@gmail.com>
2019-08-25 17:09:29 +0700

    Updates

---
+++ precache-manifest.8a3be77d7ff1e7700de69506d152b960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "87731cc50bee655a22f2",
+    "url": "/static/js/main.87731cc5.chunk.js"
+  },
+  {
+    "revision": "0b334eb8921cfe0098f6",
+    "url": "/static/js/1.0b334eb8.chunk.js"
+  },
+  {
+    "revision": "87731cc50bee655a22f2",
+    "url": "/static/css/main.b43a3591.chunk.css"
+  },
+  {
+    "revision": "0b334eb8921cfe0098f6",
+    "url": "/static/css/1.b2594f04.chunk.css"
+  },
+  {
+    "revision": "7da66db74506a267557d94e761d1794d",
+    "url": "/index.html"
+  }
+];
