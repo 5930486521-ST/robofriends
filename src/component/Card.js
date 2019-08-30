8c3f5e4fa13162432231bc0ade2d116bb5477145
gems.sirawit <sirawit.james@gmail.com>
2019-08-30 17:43:45 +0700

    lighthouse: SEO, Accessability, Performance

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-const Card = ({id, name, username, email}) => {
-    return (<div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-        <img src={"https://robohash.org/"+ id} alt = "not found"></img>
-        <div>
-            <h2>{username}</h2>
-            <h3>Name: {name}</h3>
-            <h4>Email: {email}</h4>
-        </div>
-    </div>);
-}
+const Card = ({ id, name, username, email }) => {
+  return (
+    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+      <img
+        src={`https://robohash.org/${id}?size=200x200`}
+        alt="not found"
+      ></img>
+      <div>
+        <h2>{username}</h2>
+        <h3>Name: {name}</h3>
+        <h4>Email: {email}</h4>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- src/component/SearchBox.js
+++ src/component/SearchBox.js
@@ -3,6 +3,6 @@ import React from "react"
 export const SearchBox = ({handler}) =>{
     return <div className = "p2">
         <input onChange={handler} className="pa3 bs b--green bg-lightest-blue"  
-                type="search" placeholder="seach robots by name"></input>
+                type="search" placeholder="search robots by name" aria-label="search box for finding robots"></input>
     </div>
 }
